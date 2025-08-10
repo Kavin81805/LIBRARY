a3b4dc71d0f7066642b5322d428f1533faae87a3
Kavin81805 <tysonkavin35@gmail.com>
2025-08-10 17:12:45 +0530

    FIX : update's in code

--- library_system.java
+++ library_system.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 public class library_system {
     public static void main(String[] args) {
         Scanner sc = new  Scanner(System.in);
+        Members m = new Members(sc);
+        m.arr_menber();
+        m.arr_dis_member();
     }
 }
 
@@ -11,6 +14,7 @@ class Members {
     Scanner sc;
     String m_name , m_id , p_num ;
     int n;
+    Members [] member;
     Members(Scanner scanner){
         this.sc = scanner;
     }
@@ -24,20 +28,25 @@ class Members {
         p_num = sc.nextLine();
         System.out.println("\nSUCCESS FULLY ADDED.....\n------------------------------------------");
     }
+    void Diss_members(){
+        System.out.println("\n\n------------------------------------------\n\n==========| MEMBER |==========\n\n| NAME : "+m_name+" |\n| ID : "+m_id+" |\n| PHONE NUMBER : "+p_num+" |\n\n------------------------------------------");
+    }
     void arr_menber(){
         System.out.print("HOW MANY MEMBERS ID YOU WANT TO CREATE : ");
         n = sc.nextInt();
         sc.nextLine();
-        Members [] member = new Members[n];
+           member = new Members[n];
         for(int i = 0 ; i < n ; i++){
            System.out.println("==========|FORM "+(i+1)+" |==========");
            member[i] = new Members(sc);
            member[i].add_menber();
         } 
     }
-    void dis_member(){
-        System.out.println("\n------------------------------------------\n==========| MEMBERS |===========\n");
-
+    void arr_dis_member(){
+        System.out.println("\n\n==========| MEMBERS |===========\n");
+        for(int i = 0 ; i < n ; i++){
+           member[i].Diss_members();
+        }
            
         }
     }
@@ -47,6 +56,7 @@ class Books {
     Scanner sc;
     String ISBN ,title , author , price;
     int n;
+    Books [] book;
     Books(Scanner sc){
         this.sc = sc;
     }
@@ -62,16 +72,25 @@ class Books {
         System.out.print("ENETR THE PRICE : ");
         price = sc.nextLine();  
     }
+    void diss_books(){
+        System.out.println("\n\n------------------------------------------\n==========| BOOK |==========\n| BOOK NAME : "+title+" |\n| AUTHOR NAME : "+author+" |\n| ISBN NUMBER : "+ISBN+" |\n| PRICE : "+price+" |\n\n------------------------------------------");
+    }
     void arr_book(){
         System.out.print("HOW MANY MEMBERS ID YOU WANT TO CREATE : ");
         n = sc.nextInt();
         sc.nextLine();
-        Books [] book = new Books[n];
+           book = new Books[n];
         for(int i = 0 ; i < n ; i++){
            System.out.println("==========|FORM "+(i+1)+" |==========");
            book [i] = new Books(sc);
            book [i].add_book();
         }       
+    }
+    void arr_dis_books(){
+        System.out.println("\n\n==========| BOOKS |===========\n");
+        for(int i = 0 ; i < n; i++ ){
+           book[i].diss_books();
+        }
     }
 }
 class Library_Manager {
